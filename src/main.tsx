92c9ef2d06d7a27ec59acdc4a4b32ee1e69cf999
MinilikBelachew <belachewr35@gmail.com>
2026-01-05 04:40:21 -0800

    update scanner

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,16 @@ import './index.css'
 import { ThemeProvider } from './context/ThemeProvider';
 import './i18n';
 
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm('New content available. Reload?')) {
+      updateSW(true)
+    }
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <React.Suspense fallback={<div className="flex h-screen w-full items-center justify-center">Loading...</div>}>
